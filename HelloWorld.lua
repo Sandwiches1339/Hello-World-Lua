0f0ea54d120e4c3ff863b4858a6a1171aed9ed1d
sandwiches <158267724+Sandwiches1339@users.noreply.github.com>
2024-01-31 16:04:48 +0700

    Hello World Lua
    Hello World Lua

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -2,4 +2,8 @@
 --Hello World Lua
 -- sandwiches's Hello World Lua, 2023-2024
 
-print("Hello World!")
+local HelloWorldLua = "Hello World!"
+
+for i = 1,#HelloWorldLua,1 do
+  print(string.sub(HelloWorldLua, i, i))
+end
